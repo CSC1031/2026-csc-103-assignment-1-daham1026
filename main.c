68e10e6affcc000259bed2e4622daafeca23358b
daham1026 <dahamuniv@gmail.com>
2026-06-08 09:31:22 +0530

    adding average and total

--- main.c
+++ main.c
@@ -11,7 +11,18 @@ int main()
     float average;
 
     printf("\tStudent grade analyzer!\n\n");
+
     printf("enter mathematics marks: ");
     scanf("%d", &mathematics);
+
+    printf("enter science marks: ");
+    scanf("%d", &science);
+
+    printf("enter english marks: ");
+    scanf("%d", &english);
+
+    total= maths + science + english;
+    average = total/3.0;
+
     return 0;
 }
