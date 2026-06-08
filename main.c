56fad6e23171b23f17d23fd62048fb4755651bde
daham1026 <dahamuniv@gmail.com>
2026-06-08 09:06:30 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int mathematics;
+    int english;
+    int science;
+    int total;
+    char grade;
+    float average;
+
+    printf("\tStudent grade analyzer!\n\n");
+    printf("enter mathematics marks: ");
+    scanf("%d", &mathematics);
+    return 0;
+}
